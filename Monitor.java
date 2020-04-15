46c4962746da2346bf23a492b51ddda4a9a43644
MANI AHMADI ARSHAM <mani.ahmadi.arsham@lightspeedhq.com>
2020-04-15 19:49:23 -0400

    new class

--- Monitor.java
+++ Monitor.java
@@ -5,54 +5,6 @@
  * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
  */
 public class Monitor {
-	private class Chopstick {
-		public boolean pickedUp;
-		public int lastPickedUpBy;
-
-		public Chopstick() {
-			pickedUp = false;
-			lastPickedUpBy = 0;
-		}
-
-		/**
-		 * Determines whether a chopstick was last picked up by a philosopher
-		 * 
-		 * @param piTID Thread ID of the philosopher
-		 * @return Whether the philosopher last picked by the chopstick
-		 */
-		public boolean lastPickedUpByMe(final int piTID) {
-			return lastPickedUpBy == piTID;
-		}
-
-		/**
-		 * Determines whether a chopstick is currently picked up by another philosopher
-		 * 
-		 * @param piTID Thread ID of the philosopher
-		 * @return True if the chopstick is picked up and the philosopher ID does not
-		 *         match
-		 */
-		public boolean pickedUpByAnother(final int piTID) {
-			return lastPickedUpBy != piTID && pickedUp;
-		}
-
-		/**
-		 * Pick up a chopstick and set the new philosopher
-		 * 
-		 * @param piTID Thread ID of the philosopher
-		 */
-		public void pickUp(final int piTID) {
-			pickedUp = true;
-			lastPickedUpBy = piTID;
-		}
-
-		/**
-		 * Put a chopstick down
-		 */
-		public void putDown() {
-			pickedUp = false;
-		}
-	}
-
 	/*
 	 * ------------ Data members ------------
 	 */
